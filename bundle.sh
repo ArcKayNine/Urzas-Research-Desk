4dc21c4cb7a7108f46ee39a55d65ba046d731f68
JEGolding <j.golding996@gmail.com>
2025-03-11 22:59:47 +1100

    importing panel in workflow

--- bundle.sh
+++ bundle.sh
@@ -1,15 +1,18 @@
 # Convert to pyodide
 #
+echo "Converting to pyodide..."
 panel convert index.py --to pyodide-worker --out docs/
 
 # Add in the header information we need.
 # This includes unpacking the zipped data and 
 # adding the google analytics tag.
 #
+echo "Adding heqaders..."
 python bundle.py
 
 # Run the processing and save in a zip.
 #
+echo "Processing data..."
 python process_data.py
 zip docs/processed_data.zip processed_data/
 
