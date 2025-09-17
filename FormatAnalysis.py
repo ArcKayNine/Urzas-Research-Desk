445f3e61c559239e4e85da4e72b43452b22d81eb
JEGolding <j.golding996@gmail.com>
2025-09-18 00:36:10 +1000

    Requirements update

--- FormatAnalysis.py
+++ FormatAnalysis.py
@@ -2,7 +2,7 @@
 from tcg_research_desk import load_data, create_dashboard
 
 
-if __name__ == '__main__':
-    df, X, res_df, vocabulary, oracleid_lookup, cards_data = load_data()
-    dashboard = create_dashboard(df, res_df, X, vocabulary, oracleid_lookup, cards_data)
-    dashboard.servable()
+# if __name__ == '__main__':
+df, X, res_df, vocabulary, oracleid_lookup, cards_data = load_data()
+dashboard = create_dashboard(df, res_df, X, vocabulary, oracleid_lookup, cards_data)
+dashboard.servable()
