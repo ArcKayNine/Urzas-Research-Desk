138e3e67d35645e1ce68bf28915420f7b58dfb74
JEGolding <j.golding996@gmail.com>
2025-09-21 21:20:23 +1000

    shape logging

--- FormatAnalysis.py
+++ FormatAnalysis.py
@@ -651,5 +651,6 @@ from tcg_research_desk import load_data, create_dashboard
 
 # if __name__ == '__main__':
 df, X, res_df, vocabulary, oracleid_lookup, cards_data = load_data()
+print(f'{df.shape=}, {X.shape=}, {res_df.shape=}')
 dashboard = create_dashboard(df, res_df, X, vocabulary, oracleid_lookup, cards_data)
 dashboard.servable()
