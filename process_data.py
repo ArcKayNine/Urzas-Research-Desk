ff0e9adf5b3856b1a41b2c2d2832a6858895f2c3
JEGolding <j.golding996@gmail.com>
2025-09-21 15:56:05 +1000

    last standard change

--- process_data.py
+++ process_data.py
@@ -12,6 +12,7 @@ from datetime import datetime, timedelta
 # from tqdm import tqdm
 
 from tcg_research_desk import process_mtg_data
+from tcg_research_desk.process_data import get_last_standard_change
 
 def fuzzy_join(df1, df2):
     """
@@ -364,4 +365,9 @@ if __name__ == '__main__':
     parser.add_argument("format", help="Format to process", default='Modern')
     args = parser.parse_args()
 
-    process_mtg_data(fmt=args.format)#lookback_days=30)
+    if args.format == "Standard":
+        last_date, lookback_days = get_last_standard_change()
+    else:
+        lookback_days = 182
+
+    process_mtg_data(fmt=args.format, lookback_days=lookback_days)
